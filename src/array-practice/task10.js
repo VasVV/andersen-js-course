e7eefbe813d1b2485c601e8802d265d12e43486c
VasVV <radioheadboy0@gmail.com>
2023-10-04 19:47:40 +0300

    task10

--- src/array-practice/task10.js
+++ src/array-practice/task10.js
@@ -10,11 +10,20 @@
 
 // Раскомментировать нижнее, при начале реализации
 
-// const meetups = [
-//   { name: 'JavaScript', isActive: true, members: 100 },
-//   { name: 'Angular', isActive: true, members: 900 },
-//   { name: 'Node', isActive: false, members: 600 },
-//   { name: 'React', isActive: true, members: 500 },
-// ];
+const meetups = [
+  { name: 'JavaScript', isActive: true, members: 100 },
+  { name: 'Angular', isActive: true, members: 900 },
+  { name: 'Node', isActive: false, members: 600 },
+  { name: 'React', isActive: true, members: 500 },
+];
 
-// function membersOnActiveMeetups(meetups) {}
+export default function membersOnActiveMeetups(meetups) {
+  let m = 0;
+  // eslint-disable-next-line no-restricted-syntax
+  for (const value in meetups) {
+    if (meetups[value].isActive) {
+      m += meetups[value].members;
+    }
+  }
+  return m;
+}
